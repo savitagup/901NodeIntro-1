d9fbdff7a8daadec3d6f35fd7d697ec8f18a1d4c
Judy Lipinski <judy@karmoxie.com>
2018-08-26 09:20:14 -0400

    mv ch4 and 5 labs

--- Labs/Ch04-FileAccess/Ex02-ReadFilePromises/Begin/promise-read.js
+++
@@ -1,4 +0,0 @@
-const Promise = require("bluebird");
-const fs = Promise.promisifyAll(require("fs"));
-
-// Write your code here
--- Labs/Ch05-DBAccess/Begin/database-practice.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const Promise = require("bluebird");
-const knex = require("knex");
-let db = knex(require("./knexfile"));
-
-//add more code here
