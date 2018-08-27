fef1dd46167f779376dacc204622d56c2085e660
Judy Lipinki <judy@karmoxie.com>
2018-08-27 06:38:14 -0400

    updated comments

--- Solutions/Ch02/modules/main.js
+++ Solutions/Ch02/modules/main.js
@@ -1,9 +1,10 @@
 const add = require('./add.js');
 const Calculator = require('./calculator.js');
 
+//use function
 console.log('5+7=', add(5,7));
 
-
+// use class
 const myCalc = new Calculator();
 
 console.log(`1+2=`,myCalc.add(1,2));
