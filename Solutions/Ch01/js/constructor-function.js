4f65b2b02096fb684d4cb6a9ce84b53297b650e8
Judy Lipinki <judy@karmoxie.com>
2018-08-27 15:00:27 -0400

    added this.fname to solution

--- Solutions/Ch01/js/constructor-function.js
+++ Solutions/Ch01/js/constructor-function.js
@@ -5,10 +5,11 @@ function Person(fname, lname) {
     this.fname = fname;
     this.lname = lname;
     this.getFullName = function() {
-        return `${fname} ${lname}`
+        return `${this.fname} ${this.lname}`
     }
 }
 
 let person1 = new Person(firstName, lastName);
+person1.fname = 'xxxxx';
 console.log(person1.getFullName());
 
