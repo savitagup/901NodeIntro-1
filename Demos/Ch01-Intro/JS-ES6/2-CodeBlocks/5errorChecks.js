61f2cb6443629748db803147140e345a16b8960f
Judy Lipinski <judy@karmoxie.com>
2018-12-10 11:26:30 -0500

    updated code to set 0 and 50000 as bounds

--- Demos/Ch01-Intro/JS-ES6/2-CodeBlocks/5errorChecks.js
+++ Demos/Ch01-Intro/JS-ES6/2-CodeBlocks/5errorChecks.js
@@ -1,14 +1,17 @@
-var check = function(num) {
-  if (num < MIN || num > MAX) {
-    throw new RangeError('Parameter must be between ' + MIN + ' and ' + MAX);
+let check = function(num) {
+  console.log(num);
+  if (num < 50000 || num > 0) {
+    throw new RangeError('Parameter must be between ' + 0 + ' and ' + 50000);
   }
+  
 };
 
 try {
   check(500);
+  check(50099);
 }
 catch (e) {
   if (e instanceof RangeError) {
-    // Handle range error
+    console.log("error",e);
   }
 }
