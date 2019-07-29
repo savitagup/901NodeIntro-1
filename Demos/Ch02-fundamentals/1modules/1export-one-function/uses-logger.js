5c9b3c6b6acecd84c8b44c6662ebd8703ae249ed
Judy Lipinki <judy@karmoxie.com>
2019-07-28 20:35:12 -0400

    formatting

--- Demos/Ch02-fundamentals/1modules/1export-one-function/uses-logger.js
+++ Demos/Ch02-fundamentals/1modules/1export-one-function/uses-logger.js
@@ -1,7 +1,6 @@
-//Best practice, use const - you wont be changing
-//the module!
+//Best practice, use const - 
+//you wont be changing the module!
 const log = require('./logger');
-
 
 //only exported one thing, can use directly
 log('something');
