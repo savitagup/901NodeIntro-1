690a1381d58a71afe67fe67cd48b271c1f7ea405
Judy Lipinki <judy@karmoxie.com>
2019-07-29 05:35:35 -0400

    switched dirs

--- Demos/Ch02-fundamentals/1modules/2shapes-export-class/shapeCalculator.js
+++ Demos/Ch02-fundamentals/1modules/2shapes-export-class/shapeCalculator.js
@@ -1,4 +1,4 @@
-const Square = require('./square.js');
+const Square = require('./square.js.js');
 const mySquare = new Square(3);
 console.log(`The area of mySquare is
              ${mySquare.area()}`);
